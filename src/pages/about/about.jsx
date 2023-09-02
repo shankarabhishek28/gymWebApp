d9f1e91e5275fb42ab48f1974e50195847ac44bf
shankarabhishek28 <127935938+shankarabhishek28@users.noreply.github.com>
2023-09-02 14:22:35 +0530

    responsive fail fixed

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -16,9 +16,11 @@ const About = () => {
     </Header>
       <section className="about_story animated">
         <div className="container about_story-container">
-         
+          <div className='about_section-image'>
             <img src={StoryImage} alt='our story image' />
-          
+          </div>
+
+
           <div className="about_section-content">
             <h1>Our Story</h1>
             <p>Lorem ipsum dolor sit amet consectetur
@@ -28,7 +30,7 @@ const About = () => {
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore minima,
               vero perspiciatis inventore blanditiis officiis!
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem molestias 
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem molestias
               reprehenderit nisi vitae dolore harum dignissimos soluta facilis fuga quod.
             </p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
@@ -55,7 +57,7 @@ const About = () => {
 
           </div>
           <div className="about_section-image v">
-              <img src={VissionImage} alt='our Vission image' />
+            <img src={VissionImage} alt='our Vission image' />
           </div>
 
         </div>
@@ -78,7 +80,7 @@ const About = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
               natus provident blanditiis impedit debitis omnis.
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. In soluta distinctio voluptatibus atque tempore consequatur inventore
-               voluptatum, et enim provident?
+              voluptatum, et enim provident?
             </p>
           </div>
 
